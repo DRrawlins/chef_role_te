db2b3c53c7f463bf0806decda8bc38bad7d3e0c8
Daniel Rawlins <danrawlins07@gmail.com>
2019-02-17 10:02:06 -0500

    added some depency fixes

--- install-fed-dependencies.sh
+++ install-fed-dependencies.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 set -e
 set -x
@@ -8,6 +7,9 @@ gem install ffi-yajl -v '2.3.1' --source 'https://rubygems.org/'
 gem install libyajl2
 sudo dnf install -y zlib-devel
 gem update
-bundle install
 sudo  gem pristine libyajl2 --version 1.2.0
-bundle update
+bundle install
+sudo gem pristine ffi --version 1.10.0
+sudo gem pristine jaro_winkler --version 1.5.2
+sudo gem pristine nokogiri --version 1.10.1
+sudo gem pristine unf_ext --version 0.0.7.5
