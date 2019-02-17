d8e3cf64fd2f9aad26a10dfd61d0d1a5614f6bdb
Daniel Rawlins <danrawlins07@gmail.com>
2019-02-17 10:03:19 -0500

    metadata bump and dependency updates

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,9 @@ maintainer_email 'danrawlins07@gmail.com'
 license          'MIT'
 description 'Installs/Configures chef_role_te'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
-depends 'chef_nexus', '~> 0.2.0'
-depends 'chef_jenkins', '~> 0.4.0'
+version '0.1.1'
+depends 'chef_nexus', '~> 0.2.1'
+depends 'chef_jenkins', '~> 0.4.1'
 
 %w{fedora}.each do |os|
   supports os
